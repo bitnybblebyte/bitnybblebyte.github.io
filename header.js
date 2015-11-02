8f9c7177a1ab15221a2d3541722896f0d4912d21
jason-yang31415 <jason.yang31415@gmail.com>
2015-11-02 17:28:53 -0500

    Added common header, contact page

---
+++ header.js
@@ -0,0 +1,15 @@
+var html =		"<div id='nav'>"
+	+ "\n" + 	"      <ul>"
+	+ "\n" + 	"        <li><a href='index.html'>Home</a></li>"
+	+ "\n" +	"        <li><a href='http://github.com/bitnybblebyte'>Repositories</a></li>"
+	+ "\n" +	"        <li><a href='contact.html'>Contact</a></li>"
+	+ "\n" +	"      </ul>"
+	+ "\n" +	"    </div>"
+	+ "\n" +	"    <div id='header'>"
+	+ "\n" +	"      <img src='images/bnb_256.png' id='logo' />"
+	+ "\n" +	"      <h1 class='center'>"
+	+ "\n" +	"        Bit.Nybble.Byte"
+	+ "\n" +	"      </h1>"
+	+ "\n" +	"    </div>";
+
+document.write(html);
