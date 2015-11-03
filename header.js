490d14d800e57bf80a66cb7f96a57ea618f59879
jason-yang31415 <jason.yang31415@gmail.com>
2015-11-02 20:24:29 -0500

    Added mobile optimizations

--- header.js
+++ header.js
@@ -1,4 +1,6 @@
-var html =		"<div id='nav'>"
+var html;
+if (!mobile){
+  html =		"<div id='nav'>"
 	+ "\n" + 	"      <ul>"
 	+ "\n" + 	"        <li><a href='index.html'>Home</a></li>"
     + "\n" + 	"        <li><a href='about.html'>About</a></li>"
@@ -7,10 +9,27 @@ var html =		"<div id='nav'>"
 	+ "\n" +	"      </ul>"
 	+ "\n" +	"    </div>"
 	+ "\n" +	"    <div id='header'>"
-	+ "\n" +	"      <img src='images/bnb_256.png' id='logo' />"
-	+ "\n" +	"      <h1 class='center'>"
+    + "\n" +	"      <img src='images/bnb_256.png' id='logo' />"
+    + "\n" +	"      <h1 class='center'>"
 	+ "\n" +	"        Bit.Nybble.Byte"
 	+ "\n" +	"      </h1>"
 	+ "\n" +	"    </div>";
+}
+else {
+  html =		"<div id='nav' class='nav_collapsed'>"
+    + "\n" + 	"      <img src='images/nav.png' style='position: absolute;'>"
+	+ "\n" + 	"      <ul class='hidden'>"
+	+ "\n" + 	"        <li><a href='index.html'>Home</a></li>"
+    + "\n" + 	"        <li><a href='about.html'>About</a></li>"
+	+ "\n" +	"        <li><a href='http://github.com/bitnybblebyte'>Repositories</a></li>"
+	+ "\n" +	"        <li><a href='contact.html'>Contact</a></li>"
+	+ "\n" +	"      </ul>"
+	+ "\n" +	"    </div>"
+	+ "\n" +	"    <div id='header'>"
+    + "\n" +	"      <h1 class='center'>"
+	+ "\n" +	"        Bit.Nybble.Byte"
+	+ "\n" +	"      </h1>"
+	+ "\n" +	"    </div>";
+}
 
 document.write(html);
